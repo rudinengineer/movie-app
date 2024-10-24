419ce96e03c9e0fe978df52f9ef2bb745527a06c
Rudin Engineer <rudinengineer@gmail.com>
2024-10-24 19:51:44 +0700

    redeploy

--- src/app/movie/[movieId]/page.tsx
+++ src/app/movie/[movieId]/page.tsx
@@ -1,37 +1,36 @@
-"use server"
-import { useRouter } from 'next/router'
 import React from 'react'
 import MovieDetail from '~/components/ui/movie-detail'
+import { POSTER_BASEURL } from '~/constants/tmdb'
+import { sendRequestTMDB } from '~/lib/tmdb'
+import { MovieType } from '~/types/movie'
 // import { POSTER_BASEURL } from '~/constants/tmdb'
 // import { sendRequestTMDB } from '~/lib/tmdb'
 // import { MovieType } from '~/types/movie'
 
 type Props = {
-    // params: {
-    //     movieId: any
-    // }
+    params: {
+        movieId: any
+    }
 }
 
-// export async function generateMetadata({params}: Props) {
-//     const response = await sendRequestTMDB(`/movie/${params.movieId}`)
-//     const data: MovieType = await response?.data
+export async function generateMetadata({params}: Props) {
+    const response = await sendRequestTMDB(`/movie/${params.movieId}`)
+    const data: MovieType = await response?.data
 
-//     return {
-//         title: data.title ?? data.original_title,
-//         description: data.overview,
-//         openGraph: {
-//             title: data.title ?? data.title,
-//             images: POSTER_BASEURL + data.poster_path
-//         }
-//     }
-// }
+    return {
+        title: data.title ?? data.original_title,
+        description: data.overview,
+        openGraph: {
+            title: data.title ?? data.title,
+            images: POSTER_BASEURL + data.poster_path
+        }
+    }
+}
 
-export default function Page({}: Props) {
-  const router = useRouter()
-
+export default function Page({params}: Props) {
   return (
-    router.query?.movieId && (
-        <MovieDetail movieId={router.query.movieId} />
+    params?.movieId && (
+        <MovieDetail movieId={params.movieId} />
     )
   )
 }
